2db094c36c81729e3af2c04e03ebaea2bccfe3c0
Fatern <jovanwibisono12@gmail.com>
2024-11-04 19:10:50 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "poppins": ['poppins']
+      },
+      colors: {
+        putih: "#EEF4FF",
+        biru: "#2D76F9",
+      },
+    },
+  },
+  plugins: [],
+};
